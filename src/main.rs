7230fda2dd39bc38a2148fa370c7616f64bd5d41
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-01-02 18:09:09 +0100

    added output image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
+use std::fs;
+
+
 mod vec3;
 
-fn main() {
-    let vec1 = vec3::init(1.0, 2.0, 3.0);
-    let vec2 = vec3::init(1.0, 2.0, 3.0);
+fn main(){
+    let image_width = 256;
+    let image_height = 256;
 
-    let _vec3 = vec3::add(vec1, vec2);
+    let mut img_str = "".to_owned();
 
-    println!("Hello, world!");
+    println!("P3\n{} {}\n255\n", image_width, image_height);
+    img_str.push_str(&format!("P3\n{} {}\n255\n", image_width, image_height));
+
+
+    for j in (0..image_height).rev() {
+        //println!("j: {}", j);
+        for i in 0..image_width {
+            //println!("i: {}", i);
+
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {} \n", ir, ig, ib);
+            img_str.push_str(&format!("{} {} {} \n", ir, ig, ib));
+        }
+    }
+
+    fs::write("image.ppm", img_str).expect("Unable to write file");
 }
