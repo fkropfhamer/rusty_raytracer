e5d6c9c32d1757d06163ccd50f94fa17b07d394f
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-01-02 17:14:56 +0100

    added dot and tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod vec3;
 
 fn main() {
+    let vec1 = vec3::init(1.0, 2.0, 3.0);
+    let vec2 = vec3::init(1.0, 2.0, 3.0);
+
+    let _vec3 = vec3::add(vec1, vec2);
+
     println!("Hello, world!");
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -5,6 +5,14 @@ pub struct Vec3 {
     z: f32,
 }
 
+pub fn init(x: f32, y: f32, z: f32) -> Vec3 {
+    Vec3 {
+        x,
+        y,
+        z,
+    }
+}
+
 pub fn add(vec1: Vec3, vec2: Vec3) -> Vec3 {
     let x = vec1.x + vec2.x;
     let y = vec1.y + vec2.y;
@@ -15,6 +23,10 @@ pub fn add(vec1: Vec3, vec2: Vec3) -> Vec3 {
         y,
         z,
     };
+}
+
+pub fn dot(vec1: Vec3, vec2: Vec3) -> f32 {
+    return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
 }
 
 #[cfg(test)]
@@ -42,5 +54,22 @@ mod tests {
         };
 
         assert_eq!(add(vec1, vec2), vec3)
+    }
+
+    #[test]
+    fn test_init() {
+        let vec = init(1.0, 2.0, 3.0);
+
+        assert_eq!(vec.x, 1.0);
+        assert_eq!(vec.y, 2.0);
+        assert_eq!(vec.z, 3.0);
+    }
+
+    #[test]
+    fn test_dot() {
+        let vec1 = init(1.0, 2.0, 3.0);
+        let vec2 = init(1.0, 2.0, 3.0);
+
+        assert_eq!(dot(vec1, vec2), 14.0)
     }
 }
