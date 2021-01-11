f368b224fe6865ecf3b3720a2948a581e911514e
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-01-11 21:37:53 +0100

    added sphere

--- src/ray.rs
+++ src/ray.rs
@@ -13,7 +13,19 @@ pub fn init(origin: vec3::Vec3, direction: vec3::Vec3) -> Ray {
     }
 }
 
+fn hit_sphere(center: &vec3::Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - *center;
+    let a = vec3::dot(r.direction, r.direction);
+    let b = 2.0 * vec3::dot(oc, r.direction);
+    let c = vec3::dot(oc, oc) - radius * radius;
+    let disriminant = b * b - 4.0 * a * c;
+    disriminant > 0.0
+}
+
 pub fn ray_color(ray: Ray) -> vec3::Vec3 {
+    if hit_sphere(&vec3::init(0.0, 0.0, -1.0), 0.5, &ray) {
+        return vec3::init(1.0, 0.0, 0.0);
+    }
     let unit_direction = vec3::unit_vector(&ray.direction);
     let t = 0.5 * (unit_direction.get_y() + 1.0);
 
